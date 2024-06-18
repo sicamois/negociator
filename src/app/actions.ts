cdf667c4b312ad8677cf54d91f943bc09ffeece7
Po <sicamois@gmail.com>
2024-06-19 01:57:01 +0200

    add urssaf rules

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { salariesFields, autoEntrepreneurFields } from '@/app/fields';
+import Engine from 'publicodes';
+import rules from 'modele-social';
 
 function setFormDataValue(
   formData: FormData,
@@ -29,77 +31,167 @@ export async function calculate(
   withAcre: boolean,
   fromInput: string
 ) {
-  console.log('withAcre', withAcre);
   if (
     !salariesFields.includes(fromInput) &&
     !autoEntrepreneurFields.includes(fromInput)
   ) {
     return formData;
   }
+  const engine = new Engine(rules);
+  const baseSituation: Record<string, any> = {
+    'salarié . contrat': "'CDD'",
+    'salarié . contrat . CDD . durée': 1,
+    'salarié . contrat . CDD . congés pris': '0 jours ouvrés',
+    'salarié . contrat . CDD . motif': "'classique . usage'",
+    'salarié . convention collective': "'HCR'",
+    'salarié . rémunération . avantages en nature': 'oui',
+    'salarié . rémunération . avantages en nature . ntic': 'non',
+    'salarié . rémunération . avantages en nature . nourriture': 'oui',
+    'salarié . rémunération . avantages en nature . nourriture . repas par mois':
+      '21 repas/mois',
+    'établissement . commune . code postal': "'75008'",
+    'établissement . taux ATMP . taux collectif': '2.21%',
+    'impôt . méthode de calcul': "'taux personnalisé'",
+    'impôt . taux personnalisé': 10,
+    dirigeant: 'non',
+  };
 
-  const baseValue = parseInt((formData.get(fromInput) ?? '0').toString());
-
-  let salaireBrut = parseInt((formData.get('salaire_brut') ?? '0').toString());
+  const value = parseInt((formData.get(fromInput) ?? '0').toString());
+  const hourlyMultiplier =
+    value < 100
+      ? (engine.evaluate('salarié . contrat . temps de travail')
+          .nodeValue as number)
+      : 1;
 
   switch (fromInput) {
     case 'cout_employeur':
-      salaireBrut = baseValue / 1.18765;
+      engine.setSituation({
+        ...baseSituation,
+        'salarié . coût total employeur': value * 12 * hourlyMultiplier,
+      });
       break;
     case 'salaire_brut':
-      salaireBrut = baseValue;
+      engine.setSituation({
+        ...baseSituation,
+        'salarié . contrat . salaire brut': value * hourlyMultiplier,
+      });
       break;
     case 'salaire_net':
-      salaireBrut = baseValue / 0.77577;
+      engine.setSituation({
+        ...baseSituation,
+        'salarié . rémunération . net . à payer avant impôt':
+          value * hourlyMultiplier,
+      });
       break;
     case 'salaire_net_apres_impot':
-      salaireBrut = baseValue / 0.76342;
+      engine.setSituation({
+        ...baseSituation,
+        'salarié . rémunération . net . payé après impôt':
+          value * hourlyMultiplier,
+      });
       break;
     default:
+      engine.setSituation({
+        ...baseSituation,
+        'salarié . coût total employeur':
+          parseInt((formData.get('cout_employeur') ?? '0').toString()) *
+          12 *
+          hourlyMultiplier,
+      });
       break;
   }
 
   const newFormData = new FormData();
-  setFormDataValue(newFormData, 'cout_employeur', salaireBrut * 1.18765);
-  setFormDataValue(newFormData, 'salaire_brut', salaireBrut);
-  setFormDataValue(newFormData, 'salaire_net', salaireBrut * 0.77577);
+  setFormDataValue(
+    newFormData,
+    'cout_employeur',
+    (engine.evaluate('salarié . coût total employeur').nodeValue as number) /
+      hourlyMultiplier
+  );
+  setFormDataValue(
+    newFormData,
+    'salaire_brut',
+    (engine.evaluate('salarié . contrat . salaire brut').nodeValue as number) /
+      hourlyMultiplier
+  );
+  setFormDataValue(
+    newFormData,
+    'salaire_net',
+    (engine.evaluate('salarié . rémunération . net . à payer avant impôt')
+      .nodeValue as number) / hourlyMultiplier
+  );
   setFormDataValue(
     newFormData,
     'salaire_net_apres_impot',
-    salaireBrut * 0.76342
+    (engine.evaluate('salarié . rémunération . net . payé après impôt')
+      .nodeValue as number) / hourlyMultiplier
   );
 
-  let coutPrestation = parseInt(
-    (formData.get('cout_prestation') ?? '0').toString()
-  );
+  // Auto-entrepreneur
+  const contratEngine = new Engine(rules);
+  const autoentrepreneurSituation = {
+    'entreprise . catégorie juridique': "'EI'",
+    'entreprise . catégorie juridique . EI . auto-entrepreneur': 'oui',
+    'entreprise . activités . service ou vente': "'service'",
+    'entreprise . date de création': '01/07/2023',
+    'dirigeant . exonérations . ACRE': withAcre ? 'oui' : 'non',
+    'établissement . commune . département': "'Paris'",
+    'entreprise . activité . nature': "'commerciale'",
+    'dirigeant . auto-entrepreneur . impôt . versement libératoire': 'oui',
+  };
 
-  if (coutPrestation === 0) {
-    coutPrestation = salaireBrut;
-  }
-
-  const tauxAcre = withAcre ? 0.106 : 0;
   switch (fromInput) {
     case 'cout_prestation':
-      coutPrestation = baseValue;
+      contratEngine.setSituation({
+        ...autoentrepreneurSituation,
+        "dirigeant . auto-entrepreneur . chiffre d'affaires":
+          value * hourlyMultiplier,
+      });
       break;
     case 'revenu_net':
-      coutPrestation = baseValue / (0.78656 + tauxAcre);
+      contratEngine.setSituation({
+        ...autoentrepreneurSituation,
+        'dirigeant . auto-entrepreneur . revenu net': value * hourlyMultiplier,
+      });
       break;
     case 'revenu_net_apres_impot':
-      coutPrestation = baseValue / (0.76956 + tauxAcre);
+      contratEngine.setSituation({
+        ...autoentrepreneurSituation,
+        'dirigeant . auto-entrepreneur . revenu net . après impôt':
+          value * hourlyMultiplier,
+      });
       break;
     default:
+      if (parseInt((formData.get('cout_prestation') ?? '0').toString()) === 0) {
+        contratEngine.setSituation({
+          ...autoentrepreneurSituation,
+          "dirigeant . auto-entrepreneur . chiffre d'affaires": engine.evaluate(
+            'salarié . coût total employeur'
+          ).nodeValue as number,
+        });
+      }
       break;
   }
-  setFormDataValue(newFormData, 'cout_prestation', coutPrestation);
+
+  setFormDataValue(
+    newFormData,
+    'cout_prestation',
+    (contratEngine.evaluate(
+      "dirigeant . auto-entrepreneur . chiffre d'affaires"
+    ).nodeValue as number) / hourlyMultiplier
+  );
   setFormDataValue(
     newFormData,
     'revenu_net',
-    coutPrestation * (0.78656 + tauxAcre)
+    (contratEngine.evaluate('dirigeant . auto-entrepreneur . revenu net')
+      .nodeValue as number) / hourlyMultiplier
   );
   setFormDataValue(
     newFormData,
     'revenu_net_apres_impot',
-    coutPrestation * (0.76956 + tauxAcre)
+    (contratEngine.evaluate(
+      'dirigeant . auto-entrepreneur . revenu net . après impôt'
+    ).nodeValue as number) / hourlyMultiplier
   );
 
   return newFormData;
