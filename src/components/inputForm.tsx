2f20c72d5ae32549b56aebe1502a8c017b4e0db8
Po <sicamois@gmail.com>
2024-06-19 13:46:15 +0200

    add spinner

--- src/components/inputForm.tsx
+++ src/components/inputForm.tsx
@@ -6,6 +6,7 @@ import { Label } from './ui/label';
 import { calculate } from '@/app/actions';
 import { salariesFieldsInfos, autoEntrepreneurFieldsInfos } from '@/app/fields';
 import { cn } from '@/lib/utils';
+import Spinner from './spinner';
 
 export default function InputForm() {
   const formRef = useRef<HTMLFormElement>(null);
@@ -45,7 +46,10 @@ export default function InputForm() {
       className='grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-20 px-4'
     >
       <div className='flex flex-col gap-4 sm:gap-8'>
-        <h2 className='text-xl text-primary font-bold'>Employé</h2>
+        <div className='flex gap-4 items-center'>
+          <h2 className='text-xl text-primary font-bold'>Employé</h2>
+          {isPending ? <Spinner className='m-0' /> : null}
+        </div>
         <div className='flex flex-col gap-4'>
           {Object.entries(salariesFieldsInfos).map(([key, label]) => (
             <div key={key} className='h-10 grid grid-cols-2 gap-4 items-center'>
@@ -54,7 +58,16 @@ export default function InputForm() {
                 id={key}
                 name={key}
                 type='number'
-                onBlur={() => calculateAction(key)}
+                onBlur={() => {
+                  if (!isPending) {
+                    calculateAction(key);
+                  }
+                }}
+                onKeyUp={({ key: inputKey }) => {
+                  if (inputKey === 'Enter' && !isPending) {
+                    calculateAction(key);
+                  }
+                }}
                 disabled={isPending}
                 defaultValue={0}
               />
@@ -63,7 +76,10 @@ export default function InputForm() {
         </div>
       </div>
       <div className='flex flex-col gap-4 sm:gap-8'>
-        <h2 className='text-xl text-primary font-bold'>Auto-entrepreneur</h2>
+        <div className='flex gap-4 justify-start'>
+          <h2 className='text-xl text-primary font-bold'>Auto-entrepreneur</h2>
+          {isPending ? <Spinner className='m-0' /> : null}
+        </div>
         <div className='flex flex-col gap-4'>
           {Object.entries(autoEntrepreneurFieldsInfos).map(
             ([key, label], index) => (
